83aa82896c3cfb4c75fc96f5a6b4487b5bda3850
John Doe <johndoe@doebrothers.com>
2018-03-12 13:25:52 +0800

    Initial commit

---
+++ springcloud-center/src/main/java/com/emrubik/springcloud/center/SpringcloudCenterApplication.java
@@ -0,0 +1,14 @@
+package com.emrubik.springcloud.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+@SpringBootApplication
+public class SpringcloudCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudCenterApplication.class, args);
+	}
+}

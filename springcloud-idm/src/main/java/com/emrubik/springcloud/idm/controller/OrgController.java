833175a316a7e24e655a5873d91b4ef529ef8738
puroc <p00u00r00>
2018-03-28 12:36:43 +0800

    1

--- springcloud-idm/src/main/java/com/emrubik/springcloud/idm/controller/OrgController.java
+++ springcloud-idm/src/main/java/com/emrubik/springcloud/idm/controller/OrgController.java
@@ -4,17 +4,18 @@ package com.emrubik.springcloud.idm.controller;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.emrubik.springcloud.dao.entity.User;
+import com.emrubik.springcloud.domain.to.base.BaseReq;
+import com.emrubik.springcloud.domain.to.base.BaseResp;
 import com.emrubik.springcloud.domain.to.base.PageResp;
 import com.emrubik.springcloud.idm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -33,7 +34,7 @@ public class OrgController {
 
     @GetMapping("/{orgId}/users")
     public @NotNull
-    ResponseEntity getUserListByOrgId(@PathVariable String orgId, @RequestParam int current,int size) throws Exception {
+    ResponseEntity getUserListByOrgId(@PathVariable String orgId, @RequestParam int current, int size) throws Exception {
         Page<User> userPage = userService.selectPage(new Page<User>(current, size), new EntityWrapper<User>().eq("org_id", orgId));
         PageResp<User> baseResp = new PageResp<User>();
         baseResp.setPayloads(userPage.getRecords());
@@ -41,5 +42,23 @@ public class OrgController {
         return ResponseEntity.ok(baseResp);
     }
 
+    @DeleteMapping("/{orgId}/users")
+    public @NotNull
+    ResponseEntity
+    deleteUserList(@PathVariable String orgId, @RequestBody BaseReq<User> baseReq) {
+        List<String> userIdList = new ArrayList<String>();
+        List<User> users = baseReq.getPayloads();
+        int size = users.size();
+        for (int i = 0; i < size; i++) {
+            userIdList.add(users.get(i).getId()+"");
+        }
+        BaseResp resp = new BaseResp();
+        boolean result = userService.deleteBatchIds(userIdList);
+        if (!result) {
+            resp.setMessage("删除失败,userIdList:"+userIdList);
+            resp.setResultCode(BaseResp.RESULT_FAILED);
+        }
+        return ResponseEntity.ok(resp);
+    }
 }
 

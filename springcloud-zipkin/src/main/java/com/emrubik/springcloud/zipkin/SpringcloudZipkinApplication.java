644d3aea761cdad7ad941071020237bfa3eaf1e3
puroc <471049931@qq.com>
2018-11-16 14:35:59 +0800

    1

--- springcloud-zipkin/src/main/java/com/emrubik/springcloud/zipkin/SpringcloudZipkinApplication.java
+++ springcloud-zipkin/src/main/java/com/emrubik/springcloud/zipkin/SpringcloudZipkinApplication.java
@@ -2,11 +2,9 @@ package com.emrubik.springcloud.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZipkinServer  //zipkin服务器 默认使用http进行通信
 public class SpringcloudZipkinApplication {
 
